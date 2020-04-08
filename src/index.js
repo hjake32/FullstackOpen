ebcb56829bf0061e3af8d0d1cd3f1632100cb635
Jake Henkel <h.jake32@gmail.com>
2020-04-07 22:13:12 -0400

    Through part1d, on excercise 1.10

--- src/index.js
+++ src/index.js
@@ -1,37 +1,63 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-
-const App = (props) => {
-  const [counter, setCounter] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-
-  return (
-    <div>
-      <Display counter = {counter} />
-      <Button handleClick ={increaseByOne} text = "Plus" />
-      <Button handleClick ={decreaseByOne} text = "Minus" />
-      <Button handleClick ={setToZero} text = "Zero" />
-      
-    </div>
-  )
-}
-
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
 
 const Button = (props) => {
   return (
-    <button onClick={props.handleClick}>
+    <button onClick= {props.handleClick}>
       {props.text}
     </button>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Stats = (props) => {
+  const allCount = props.goodCount + props.neutralCount + props.badCount
+
+  if (allCount === 0){
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
+  const average = (props.goodCount - props.badCount) / allCount
+  return (
+    <div>
+      <p>good {props.goodCount}</p> 
+      <p>neutral {props.neutralCount}</p>
+      <p>bad {props.badCount}</p>
+      <p>all {props.goodCount + props.neutralCount + props.badCount}</p>
+      <p>average {average}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => {
+    setGood(good + 1)
+  }
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const badClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick= {goodClick} text = "good" />
+      <Button handleClick= {neutralClick} text = "neutral" />
+      <Button handleClick= {badClick} text = "bad" />
+      <h1>statistics</h1>
+      <Stats goodCount= {good} neutralCount={neutral} badCount={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
