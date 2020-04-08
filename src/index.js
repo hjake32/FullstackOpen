14b7fa864965088d45042eb414d795c0d9604d42
Jake Henkel <h.jake32@gmail.com>
2020-04-07 22:32:36 -0400

    Through part 1d, exercise 1.11

--- src/index.js
+++ src/index.js
@@ -10,23 +10,8 @@ const Button = (props) => {
 }
 
 const Stats = (props) => {
-  const allCount = props.goodCount + props.neutralCount + props.badCount
-
-  if (allCount === 0){
-    return (
-      <div>No feedback given</div>
-    )
-  }
-
-  const average = (props.goodCount - props.badCount) / allCount
   return (
-    <div>
-      <p>good {props.goodCount}</p> 
-      <p>neutral {props.neutralCount}</p>
-      <p>bad {props.badCount}</p>
-      <p>all {props.goodCount + props.neutralCount + props.badCount}</p>
-      <p>average {average}</p>
-    </div>
+    <div>{props.text} {props.value}</div>
   )
 }
 
@@ -35,25 +20,48 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const goodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
   const neutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
   const badClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
+     
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick= {goodClick} text = "good" />
-      <Button handleClick= {neutralClick} text = "neutral" />
-      <Button handleClick= {badClick} text = "bad" />
-      <h1>statistics</h1>
-      <Stats goodCount= {good} neutralCount={neutral} badCount={bad} />
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick= {goodClick} text = "good" />
+        <Button handleClick= {neutralClick} text = "neutral" />
+        <Button handleClick= {badClick} text = "bad" />
+      </div>
+      {all ? 
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td><Stats text ="good" value = {good} /></td>
+            </tr>
+            <tr>
+              <td><Stats text ="bad" value = {bad} /></td>
+            </tr>
+              <td><Stats text ="all" value = {all} /></td>
+            <tr>
+              <td><Stats text= "average" value ={ good - bad / all} /></td>
+            </tr>
+          </tbody>
+        </table>
+      </div> : "No feedback given" }
     </div>
   )
 }
