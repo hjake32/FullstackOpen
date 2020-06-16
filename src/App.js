1e5d0569f78490d7ee3a909ed218225f8dcbee3d
Jake Henkel <h.jake32@gmail.com>
2020-06-15 23:03:36 -0400

    Through 2c. Added json-server to serve notes, axios to fetch notes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => { 
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(false)
 
@@ -18,6 +19,17 @@ const App = (props) => {
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
