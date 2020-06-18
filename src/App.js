8d3340e1e240aedccafab57e2110f403931ccb6a
Jake Henkel <h.jake32@gmail.com>
2020-06-18 14:53:47 -0400

    Factor out axios services

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => { 
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(false)
 
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
+
   const toggleImportanceOf = (id) => {
-    const url = 'http://localhost:3001/notes/${id}'
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
 
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
-    })
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
   }
 
   const addNote = (event) => {
@@ -22,27 +32,16 @@ const App = () => {
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
+      important: Math.random() > .5
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
-        setNewNote('')
-      })
+    noteService
+    .create(noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
-  
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
